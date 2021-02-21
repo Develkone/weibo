d76b20b68d27d7441fc00d87d9521277264456d6
Develkone <805310424@qq.com>
2021-02-21 07:28:53 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::paginate(6);
+        $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
 
@@ -53,13 +53,13 @@ class UsersController extends Controller
         return redirect()->route('users.show', [$user]);
     }
 
-    function edit(User $user)
+    public function edit(User $user)
     {
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
-    function update(User $user, Request $request)
+    public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
         $this->validate($request, [
@@ -77,5 +77,13 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
